07e0091a1b568310fe83c927cd0cf2ba503fdb7f
MaksyKost <Maksym03112005@gmail.com>
2025-06-14 20:36:11 +0200

    Update tree_structure.h

---
+++ src/tree_structure.h
@@ -0,0 +1,20 @@
+#ifndef TREE_STRUCTURE_H
+#define TREE_STRUCTURE_H
+
+#include <iostream>
+
+class TreeStructure {
+public:
+    // Wirtualny destruktor zapewniający poprawne zarządzanie pamięcią
+    virtual ~TreeStructure() {}
+
+    // Wirtualne metody, które muszą zostać zaimplementowane w klasach pochodnych
+    virtual void insert(int value) = 0;
+    virtual void remove(int value) = 0;
+    virtual bool search(int value) const = 0;
+
+    // Opcjonalna metoda do wyświetlania drzewa, może być zaimplementowana w podklasach
+    virtual void display() const = 0;
+};
+
+#endif // TREE_STRUCTURE_H
